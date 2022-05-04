cf7a75806eae3302cf9178b6017ef4ea44882632
Corey Gettig <coreygettig7@gmail.com>
2022-05-03 23:32:05 -0400

    still trying to get heroku to work

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    use_env_variable: 'DATABASE_URL',
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
